79e53c2f4b8176baed2dc4f0c5e4bd24030ca774
Eivind Syrdalen Dovland <eivindsd@stud.ntnu.no>
2020-02-17 15:06:52 +0100

    Delete urls.py

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views   #. = current directory
-
-urlpatterns = [
-    path('', views.home, name='blog-home')  #empty string because its the home page.
-]
-
