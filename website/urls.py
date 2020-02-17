449318d2e549d2c05004c7deb9409f7fdc7fabcf
haavarrm <haavarrm@stud.ntnu.no>
2020-02-17 22:48:24 +0100

    strikkit

---
+++ website/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path("signUp/", views.signUp, name="signUp"),
+    path("events/", views.events, name="events"),
+    path("CreateEvent/", views.events, name="CreateEvent"),
+    path("profile/", views.events, name="profile"),
+    path("signUp/", views.events, name="SignUp"),
+    path("logIn/", views.events, name="logIn")
+]
