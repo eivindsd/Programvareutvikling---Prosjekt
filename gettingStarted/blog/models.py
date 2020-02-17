b4df712e4f523eab2dab6ce0e1451b33588cb2b2
eivindsd <eivindsd@stud.ntnu.no>
2020-02-17 15:21:54 +0100

    test

--- gettingStarted/blog/models.py
+++ gettingStarted/blog/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-#OK
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
