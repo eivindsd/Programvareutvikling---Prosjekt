ffe1c7a3f575fe2576225f5c65846ce59280c1cf
haavarrm <haavarrm@stud.ntnu.no>
2020-02-26 12:34:33 +0100

    minor fix

--- users/forms.py
+++ users/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.contrib.auth import password_validation
 from django.forms import DateInput
 from django.utils.translation import gettext, gettext_lazy as _
-from website.models import Rolle, Bruker
+from website.models import Rolle, Bruker, Arrangement, ArrangementManager
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -23,14 +23,14 @@ class UserRegisterForm(UserCreationForm):
                    "Passordet ditt kan ikke være helt numerisk."),
     )
     password2 = forms.CharField(
-        label=_("Bekreft passordet ditt"),
+        label=_("Bekreft ditt passord"),
         widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
         strip=False,
         help_text=_("Gjenta passordet ditt for bekreftelse."),
     )
-    bedrift = forms.BooleanField(widget=forms.CheckboxInput(), label="Jeg er en bedrift", required=False)
-    vanligBruker = forms.BooleanField(widget=forms.CheckboxInput(), label="Jeg er en privatperson", required=False)
-    strikkeNivaa = forms.IntegerField(label="Ditt strikkenivå", required=False, help_text=_("Ditt strikkenivå; 0-100."),)
+    bedrift = forms.BooleanField(widget=forms.CheckboxInput(), label="Bedrift", required=False)
+    vanligBruker = forms.BooleanField(widget=forms.CheckboxInput(), label="Vanlig strikker", required=False)
+    strikkeNivaa = forms.IntegerField(label="Ditt strikkenivå", required=False, help_text=_("Ditt strikke nivå av 100."),)
     bursdag = forms.DateField(required=False, label="Fødselsdato", input_formats=['%d/%m/%Y'],
                               help_text=_("dd/mm/åååå"),)
     fornavn = forms.CharField(label='Fornavn')
@@ -72,3 +72,54 @@ class UserRegisterForm(UserCreationForm):
 
         # User is already saved after calling BaseUserManager function.
         return bruker
+
+
+class eventForm():
+
+    title = forms.CharField(label="'Tittel"'', strip=False)
+    time = forms.DateTimeField(label='Tidspunkt', input_formats=['%d/%m/%y %H:%M'], help_text='dd/mm/åååå tt:mm')
+    location = forms.CharField(label='Sted')
+    text = forms.CharField(widget=forms.Textarea, label='Beskrivelse')
+
+    CHOICES = [('strikkekveld', 'strikkekveld'),
+               ('kurs', 'surs'),
+               ('utfordring', 'stfordring')]
+
+    type_select = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+
+
+    def getUser(self, request):
+        current_user = request.user #Should return the user currently logged in
+        return current_user
+
+
+
+
+    class Meta():
+        model = Arrangement
+        fields = ('title', 'time', 'type_select', 'location', 'text')
+
+    def save(self, *args, **kwargs):
+        data = self.cleaned_data
+        if data['strikkekveld']:
+            arrangement = ArrangementManager.create_strikkeKveld(
+                title=data['title'],
+                innhold=data['text'],
+                forfatter=self.getUser(),
+                tidspunkt=data['time'])
+        elif data['kurs']:
+            arrangement = ArrangementManager.create_Kurs(
+                title=data['title'],
+                innhold=data['text'],
+                forfatter=self.getUser(),
+                tidspunkt=data['time'])
+        elif data['utfordring']:
+            arrangement = ArrangementManager.create_utfordring(
+                title=data['title'],
+                innhold=data['text'],
+                forfatter=self.getUser(),
+                tidspunkt=data['time']
+            )
+        return arrangement
+
+
