eff83b7b2dee188fd3d9643bb302c586cffcfb3c
haavarrm <haavarrm@stud.ntnu.no>
2020-03-04 12:45:52 +0100

    added comments

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
+"""Redirects an url-request to the correct html-template, also displays the appropriate form if needed"""
 
 def register(request):
     if request.method == 'POST':
@@ -11,10 +11,6 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
-            #messages.success(request, f'Account created for {username}!')
-            #return redirect('home')
-            #username = form.cleaned_data.get('username')
-            #messages.success(request, f'Bruker opprettet for {username} Du kan nå logge inn!')
             return redirect('login')
     else:
         form = UserRegisterForm()
